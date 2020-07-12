e1ca5fb98e35899ed7d4288ed8ef15e8c9a5b3d0
ofl <ofl@covered.jp>
2020-07-12 11:56:28 +0900

    1章2の2回目

--- 1/y5YT.py
+++ 1/y5YT.py
@@ -4,15 +4,26 @@ print('じゃんけんスタート')
 
 print('自分の手を入力してください')
 my_hand = int(input('0:グー, 1:チョキ, 2:パー'))
-you_hand = random.randint(0, 2)
+your_hand = random.randint(0, 2)
 
 if my_hand == 0:
-    if you_hand == 0:
+    if your_hand == 0:
         print('あいこ')
-    elif you_hand == 1:
+    elif your_hand == 1:
         print('勝ち')
-elif my_hand == 1:
-    if you_hand == 0:
+    elif your_hand == 2:
         print('負け')
-    elif you_hand == 1:
+elif my_hand == 1:
+    if your_hand == 0:
+        print('負け')
+    elif your_hand == 1:
+        print('あいこ')
+    elif your_hand == 2:
+        print('勝ち')
+elif my_hand == 2:
+    if your_hand == 0:
+        print('勝ち')
+    elif your_hand == 1:
+        print('負け')
+    elif your_hand == 2:
         print('あいこ')
