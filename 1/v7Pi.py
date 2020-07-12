41b4dd75f44b41ac8eb81ea9ca77639f627aa058
ofl <ofl@covered.jp>
2020-07-12 11:45:13 +0900

    1章2の1回目

--- 1/v7Pi.py
+++ 1/v7Pi.py
@@ -6,24 +6,11 @@ print('自分の手を入力してください')
 my_hand = int(input('0:グー, 1:チョキ, 2:パー'))
 you_hand = random.randint(0, 2)
 
-if my_hand == 0:
-    if you_hand == 0:
-        print('あいこ')
-    elif you_hand == 1:
-        print('勝ち')
-    elif you_hand == 2:
-        print('負け')
-elif my_hand == 1:
-    if you_hand == 0:
-        print('負け')
-    elif you_hand == 1:
-        print('あいこ')
-    elif you_hand == 2:
-        print('勝ち')
-elif my_hand == 2:
-    if you_hand == 0:
-        print('勝ち')
-    elif you_hand == 1:
-        print('負け')
-    elif you_hand == 2:
-        print('あいこ')
+result = my_hand - you_hand
+
+if result == 0:
+    print('あいこ')
+elif result == 1 or result == -2:
+    print('負け')
+else:
+    print('勝ち')
